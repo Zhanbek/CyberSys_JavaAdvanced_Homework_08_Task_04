f28886d9eb7658c5a4c367e84ffeaa37b0f8709d
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-02-24 09:27:04 +0500

    Збільшує лічильник до 9

--- src/main/java/Homework_07_Task_04/SecondClass.java
+++ src/main/java/Homework_07_Task_04/SecondClass.java
@@ -10,7 +10,7 @@ class SecondClass implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 9; i++) {
             printer.printSecond(i);
         }
     }
